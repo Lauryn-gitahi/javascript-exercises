3b02f8872cfa02eac9cb709341f42104f363159c
Lauryn-gitahi <lauryn.gitahi@gmail.com>
2020-08-06 11:24:49 +0300

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,45 @@
 function add () {
-	
+	let x=1;
+	let y =10;
+	let sum =x+y;
+	return sum;
 }
 
 function subtract () {
-	
+	let x=100;
+	let y =10;
+	let minus =x-y;
+	return minus;
 }
 
 function sum () {
-	
+	let x=1;
+	let y =10;
+	let result =x+y;
+	return result;
 }
 
 function multiply () {
-	
+	let x=1;
+	let y =10
+	let mult =x*y;
+	return mult;
 }
 
 function power() {
+	let a=10;
+	let b=2;
+	let res=Math.pow(a,b);
+	return res;
 	
 }
 
 function factorial() {
-	
+	 let ans=1;
+	let n=0;
+            for (var i = 2; i <= n; i++) 
+                ans = ans * i; 
+            return ans; 
 }
 
 module.exports = {
